4aba856b44b28f482bd2588574de9345e347126d
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-07-23 22:56:56 +0300

    Mission Passed

--- src/Child.js
+++ src/Child.js
@@ -2,10 +2,12 @@ import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor, color }) {
-  console.log(onChangeColor)
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
   return (
     <div
-      onClick={onChangeColor}
+      onClick={handleClick}
       className="child"
       style={{ backgroundColor: color }}
     />
